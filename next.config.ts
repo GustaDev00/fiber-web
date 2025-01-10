321e50e279dc6c0859257e96fac5040af69cddf7
Gustavo pontes <gustavo.4dev@gmail.com>
2025-01-10 02:24:32 -0300

    feat: integrate bundle analyzer and enhance Next.js configuration; add console removal and experimental features

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,25 @@
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   compiler: {
     styledComponents: true,
+    removeConsole: true,
   },
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX,
   trailingSlash: true,
   output: "export",
   distDir: "build",
   basePath: process.env.NEXT_PUBLIC_ASSET_PREFIX,
+  experimental: {
+    optimizePackageImports: ["react-icons/*"],
+    nextScriptWorkers: true,
+    forceSwcTransforms: true,
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
